fc6d42949356c7197561340bab42f0ab2e5f807d
KevinZucca <kev.94@live.it>
2023-03-03 16:12:42 +0100

    feat: difficulties options

--- js/script.js
+++ js/script.js
@@ -14,9 +14,24 @@
 // -all'interno specifico che se cliccata, restituisce un background (quindi uno stile nuovo) ed
 //  un numero in console
 
+//  BONUS
+/* -Aggiungere una select per la difficoltà accanto al bottone per giocare in 3 difficoltà diverse
+   -difficoltà 1: 100 caselle (10x10)
+   -difficoltà 2: 81 caselle (9x9)
+   -difficoltà 3: 49 caselle (7x7)
+*/
+
+// -Genero una select e la collego in JS dal DOM. questà dovrà collegarsi alla funzione iniziale che faceva
+//  comparire la griglia e dovrà a questo punto (la funzione) cambiare nome per definire il livello facile.
+// -In base alla scelta sul select ci saranno altre due funzioni per le rimanenti difficoltà medio e difficile
+//  dove verranno definite le caselle. 
+// -Le altre istruzioni rimarranno le stesse
+
+
 
 
 const playButtonEl = document.getElementById("play-button");
+let difficultyEl = document.getElementById("difficulty-select");
 
 playButtonEl.addEventListener("click", function(){
     generateGame();
@@ -33,7 +48,7 @@ playButtonEl.addEventListener("click", function(){
  * @returns {any}
  */
 function generateGame (gameContainer, box) {
-    
+    if (difficultyEl.value == "Facile"){
     for(let i = 1; i <= 100; i++) {
         gameContainer = document.getElementById("game-container");
         gameContainer.classList.add("game-container")
@@ -49,9 +64,52 @@ function generateGame (gameContainer, box) {
             box.style.backgroundColor = "#7fffd4";
             console.log("Il numero di questa casella è: " + box.innerText )
          });
-    }
+         
+        }
+        
+    } else if (difficultyEl.value == "Medio") {
+        for(let i = 1; i <= 81; i++) {
+            gameContainer = document.getElementById("game-container");
+            gameContainer.classList.add("game-container")
+            gameContainer.classList.add("game-container-medium")
 
-    return gameContainer
-}
+    
+            let box = document.createElement("div");
+            box.classList.add("box");
+            gameContainer.append(box);
+            
+            const cellNumber = i;
+            box.innerText = cellNumber;  
+    
+            box.addEventListener("click", function(){
+                box.style.backgroundColor = "#7fffd4";
+                console.log("Il numero di questa casella è: " + box.innerText )
+             });
+             
+            }
+                
+    } else if (difficultyEl.value == "Difficile") {
+        for(let i = 1; i <= 49; i++) {
+            gameContainer = document.getElementById("game-container");
+            gameContainer.classList.add("game-container")
+            gameContainer.classList.add("game-container-hard")
+
+    
+            let box = document.createElement("div");
+            box.classList.add("box");
+            gameContainer.append(box);
+            
+            const cellNumber = i;
+            box.innerText = cellNumber;  
+    
+            box.addEventListener("click", function(){
+                box.style.backgroundColor = "#7fffd4";
+                console.log("Il numero di questa casella è: " + box.innerText )
+             });
+             
+            }
+            return gameContainer     
+        
+    }}
 
 
